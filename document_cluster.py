e9729f5478e441b0984d20a1b97c93a00f9ebf06
Muhammad Affan Khan <maffan@folio3.com>
2018-12-11 00:15:59 +0500

    displaying top words for each cluster

--- document_cluster.py
+++ document_cluster.py
@@ -1,24 +1,27 @@
-print("Importing libraries...")
+from __future__ import print_function
+from nltk.stem.snowball import SnowballStemmer
+from sklearn.cluster import KMeans
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+import re
+import nltk
+from sklearn.metrics.pairwise import cosine_similarity
+
 # import csv
 # import numpy as np
-import pandas as pd
-# import nltk
-import re
 # import os
 # import codecs
 # import matplotlib
 # from sklearn import feature_extraction
 # import mpld3
-import nltk
-
 # import helper
+
 print("Creating output file...")
 # read summary file:
 summary = open('dataset/summary.txt', 'r').readlines()
 
 stopwords = nltk.corpus.stopwords.words('english')
 
-from nltk.stem.snowball import SnowballStemmer
 
 stemmer = SnowballStemmer("english")
 
@@ -58,30 +61,24 @@ for i in summary:
     totalvocab_tokenized.extend(allwords_tokenized)
 
 print("Creating DataFrame...")
-vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index=totalvocab_stemmed)
+vocab_frame = pd.DataFrame({'words': totalvocab_tokenized}, index=totalvocab_tokenized)
 print('there are ' + str(vocab_frame.shape[0]) + ' items in vocab_frame')
-print(vocab_frame.head())
-
+print('Here is the vocab_frame:')
+print(vocab_frame)
 print("Vectorization text frequencies...")
-from sklearn.feature_extraction.text import TfidfVectorizer
+
 
 tfidf_vectorizer = TfidfVectorizer(max_df=0.8, max_features=200000,
                                    min_df=0.1, stop_words='english',
                                    use_idf=True, tokenizer=tokenize_only, ngram_range=(1, 2))
 
 tfidf_matrix = tfidf_vectorizer.fit_transform(summary)
-print(tfidf_matrix.shape)
-
 terms = tfidf_vectorizer.get_feature_names()
-print(terms)
-
-from sklearn.metrics.pairwise import cosine_similarity
 
 dist = 1 - cosine_similarity(tfidf_matrix)
-print(dist)
 
 print("Clustering data using: K-Mean clustering...")
-from sklearn.cluster import KMeans
+
 num_clusters = 8
 
 km = KMeans(n_clusters=num_clusters)
@@ -89,10 +86,22 @@ km = KMeans(n_clusters=num_clusters)
 km.fit(tfidf_matrix)
 clusters = km.labels_.tolist()
 
-print(clusters)
-
 records = {'cluster': clusters, 'summary': summary}
 frame = pd.DataFrame(records, index=[clusters], columns=['cluster', 'summary'])
 print(frame['cluster'].value_counts())
 
 print(frame)
+
+print("Top terms per cluster:")
+print()
+# sort cluster centers by proximity to centroid
+order_centroids = km.cluster_centers_.argsort()[:, ::-1]
+print(order_centroids)
+
+for i in range(num_clusters):
+    print("Cluster %d words:" % i, end='')
+
+    for ind in order_centroids[i, :6]:  # replace 6 with n words per cluster
+        print(' %s' % vocab_frame.ix[terms[ind].split(' ')].values.tolist()[0][0].encode('utf-8', 'ignore'), end=',')
+    print()
+
